6b84466fe341bb21410cbd21f63985769583a148
Christian Estevam <christianestevamb@gmail.com>
2023-07-20 17:37:03 -0300

    feat: details services, global exceptions

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# TODO-API
+
+API RESTful criada com Spring Boot para gerenciamento de tarefas. Foi criado também um front simples para consumir a API.
+
+## Tecnologias utilizadas
+
+- Java 17
---
+++ src/main/java/com/application/todoapi/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.application.todoapi.services;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.application.todoapi.models.User;
+import com.application.todoapi.repositories.UserRepository;
+import com.application.todoapi.security.UserSpringSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if (Objects.isNull(user))
+            throw new UsernameNotFoundException("Usuário não encontrado: " + username);
+        return new UserSpringSecurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+    }
+
+}
